7283c5f7b379c3917cf21af80660c37c6416209a
Paulis Gributs <pgnmaster24@gmail.com>
2023-03-21 13:03:57 +0000

    Add test for sentiment analysis

---
+++ test/sentiment_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:negate/analyser/sentiment_analysis.dart';
+
+void main() {
+  test('Sentiment Analysis Test', () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SentimentAnalysis analyser = SentimentAnalysis();
+    await analyser.init();
+    double score = analyser.classify("I love this so much!");
+    expect(score, greaterThan(0.5));
+
+    score = analyser.classify("I hate you so much");
+    expect(score, lessThan(0.5));
+
+    score = analyser.classify("I don't like this very much, but it has some good points");
+    expect(score, greaterThan(0.5));
+
+    score = analyser.classify("This is an absolute masterpiece");
+    expect(score, greaterThan(0.6));
+
+    score = analyser.classify("This is a complete disaster");
+    expect(score, lessThan(0.4));
+  });
+}
